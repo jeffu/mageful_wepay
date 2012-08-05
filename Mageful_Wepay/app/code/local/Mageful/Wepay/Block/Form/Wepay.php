24db05d64c881655081ecee30bdf21e858194b9f
Jeff Uthaichai <jeff@uthaichai.com>
2012-08-05 09:12:46 -0400

    add redirect message in checkout page

--- Mageful_Wepay/app/code/local/Mageful/Wepay/Block/Form/Wepay.php
+++ Mageful_Wepay/app/code/local/Mageful/Wepay/Block/Form/Wepay.php
@@ -2,5 +2,11 @@
 
 class Mageful_Wepay_Block_Form_Wepay extends Mage_Payment_Block_Form {
 
+	protected function _toHtml() {
+		return parent::_toHtml() . '<ul id="payment_form_wepay" class="form-list" style="display:none;">
+<li class="form-alt">You will be redirected to WePay to complete the transaction.</li>
+</ul>';
+	}
 
 }
+
